42cfe7c96e776a9f720ebe912897a1ffae113d74
filipetenedini <tenedinifilipe@gmail.com>
2023-04-30 18:09:57 -0300

    (feat) add order endpoint

--- src/controllers/order.controller.js
+++ src/controllers/order.controller.js
@@ -1,5 +1,24 @@
+import { db } from '../config/database.js';
+
 async function sendOrder(req, res) {
-    console.log(req);
+    const { user } = req;
+    const { data, address } = req.locals;
+
+    try {
+        const cart = await db.collection('carts').findOne({ idUser: user.idUser });
+        if (!cart) return res.status(404).send('Cart not found!');
+        const { idUser, products } = cart;
+
+        const order = { idUser, data, address, products };
+        await db.collection('orders').insertOne(order);
+            await db.collection('carts').updateOne(
+                { idUser: user.idUser }, { $set: { products: [] } }
+            );
+
+        res.send(order);
+    } catch (err) {
+        console.log(err.message);
+    }
 }
 
-export default { sendOrder }
+export default { sendOrder };
--- src/schemas/order.schema.js
+++ src/schemas/order.schema.js
@@ -1,1 +1,26 @@
-// arquivo de schema de validação das orders. (Filipe)
+import Joi from 'joi';
+
+const orderSchema = Joi.object({
+    data: Joi
+        .number()
+        .required(),
+    address: Joi.object({
+        zipcode: Joi
+            .string()
+            .length(8)
+            .required(),
+        city: Joi
+            .string()
+            .required(),
+        street: Joi
+            .string()
+            .required(),
+        number: Joi
+            .string()
+            .alphanum()
+            .required(),
+    })
+    .required(),
+});
+
+export default orderSchema;
